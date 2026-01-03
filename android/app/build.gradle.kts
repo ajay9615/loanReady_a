d6607e6866238355d6cf14bd7ec65170d146dedd
ajayk2596 <ajaykumar.dev25@gmail.com>
2026-01-03 21:18:17 +0530

    Project is added

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.loanready_app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.loanready_app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+//    signingConfigs {
+//        create("release") {
+//            storeFile = file("C:/Users/suraj/key.jks")
+//            storePassword = "your_password" // ← yahan apna password daalo
+//            keyAlias = "key"
+//            keyPassword = "your_password"   // ← yahan bhi wahi password daalo
+//        }
+//    }
+
+    buildTypes {
+        getByName("release") {
+//            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
